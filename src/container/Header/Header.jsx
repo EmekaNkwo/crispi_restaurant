f51a7a5684ee9315ead6e0842fe9ffccb339d440
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-08-21 01:09:40 +0100

    small change

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <h1 className="app__header-h1">it is not just Food. It is an Experience</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Our menus feature familiar, elevated comforts including a variety of freshly brewed wines, beers, and cocktails with all-day options alongside enticing entrées like our Prime Rib Dinner, Short Rib Stroganoff and other thoughtfully sourced proteins.
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Our menu feature familiar, elevated comforts including a variety of freshly brewed wines, beers, and cocktails with all-day options alongside enticing entrées like our Prime Rib Dinner, Short Rib Stroganoff and other thoughtfully sourced proteins.
       </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
